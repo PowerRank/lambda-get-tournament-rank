0a9e61246f26b70030d51ad4b4503a034542134e
Aisha Ahmed <aisha.ahmed1@yahoo.com>
2023-10-23 11:10:16 -0500

    add functionality code (#2)
    * Update lambda_function.py
    
    * edits
    
    * add optional stage functionality
    
    * cleanup

--- lambda_function.py
+++ lambda_function.py
@@ -1,9 +1,51 @@
+import boto3
+import os
 import json
+from dynamodb_json import json_util 
+from boto3.dynamodb.conditions import Key
+
+dynamodb = boto3.resource('dynamodb')
 
 def lambda_handler(event, context):
-    # TODO implement
+    try:
+        table = dynamodb.Table(os.environ['TABLE_NAME'])
+        stageIds = []
+        try:
+            if event['queryStringParameters']['stage']:
+                response = table.get_item(
+                    Key={
+                        'PK':('Tournament#' + event['pathParameters']['tournament_id']),
+                        'SK':('Stage#' + event['queryStringParameters']['stage'])
+                    },
+                    ProjectionExpression='StageId,#n',
+                    ExpressionAttributeNames={'#n': 'Name'}
+                )
+                stageIds.append([response['Item']['StageId'], response['Item']['Name']])
+        except:
+            response = table.query(
+                ProjectionExpression='StageId,#n',
+                KeyConditionExpression=Key('PK').eq('Tournament#' + event['pathParameters']['tournament_id']),
+                ExpressionAttributeNames={'#n': 'Name'}
+            )
+            for item in response['Items']:
+                stageIds.append([item['StageId'], item['Name']])
+        ranks = {}
+        for id in stageIds:
+            response = table.query(
+                IndexName=os.environ['POINTS_LSI_NAME'],
+                ScanIndexForward=False,
+                ProjectionExpression='TeamId,#n,Points',
+                KeyConditionExpression=Key('PK').eq('Stage#'+id[0]),
+                ExpressionAttributeNames={'#n': 'Name'}
+            )
+            ranks[id[1]]=response['Items']
+        return {
+            'statusCode': 200, 
+            'body':json.dumps(json_util.loads(ranks))
+        }
+    except Exception as e:
+        print(f'Exception: {e}')
     return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda 2!')
-    }
-
+            'statusCode': 500,
+            'body': json.dumps('Invalid Request')
+        }
