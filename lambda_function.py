99c0aaef995bf3bf7375531f822b044b1361463a
aahm2221 <aisha.ahmed1@yahoo.com>
2023-10-22 22:07:56 -0500

    remove loop

--- lambda_function.py
+++ lambda_function.py
@@ -4,18 +4,24 @@ import os
 from boto3.dynamodb.conditions import Key
 
 dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table(os.environ['TABLE_NAME'])
 
 def lambda_handler(event, context):
-    response = table.query(
-        IndexName = os.environ['POINTS_LSI_NAME'],
-        ScanIndexForward=False,
-        ProjectionExpression='TeamId,#n,Points',
-        KeyConditionExpression=Key('PK').eq('Stage#'+event['stageId']),
-        ExpressionAttributeNames = {'#n': 'Name'}
-    )
-    ranks = response['Items']
-    for rank in ranks:
-        rank['TeamId']=int(rank['TeamId'])
-        rank['points']=float(rank['Points'])
-    return {'statusCode': 200, 'body':json.dumps(ranks)}
+    try:
+        table = dynamodb.Table(os.environ['TABLE_NAME'])
+        response = table.query(
+            IndexName = os.environ['POINTS_LSI_NAME'],
+            ScanIndexForward=False,
+            ProjectionExpression='TeamId,#n,Points',
+            KeyConditionExpression=Key('PK').eq('Stage#'+event['stageId']),
+            ExpressionAttributeNames = {'#n': 'Name'}
+        )
+        return {
+            'statusCode': 200, 
+            'body':json.dumps(json.loads(response['Items']))
+        }
+    except Exception as e:
+        print(f'Exception: {e}')
+    return {
+            'statusCode': 500,
+            'body': json.dumps('Invalid Request')
+        }
