e84c14a4bc2a0b1763a85195a3cef98cba44ec4e
aahm2221 <aisha.ahmed1@yahoo.com>
2023-10-23 08:43:35 -0500

    syntax fix

--- lambda_function.py
+++ lambda_function.py
@@ -13,7 +13,7 @@ def lambda_handler(event, context):
         print(event['queryStringParameters']['stage'])
         response = table.get_item(
             Key={
-                'PK':('Tournament#' + event['pathParameters']['tournament_id']),'SK':('Stage#' + event['queryStringParameters']['stage'])
+                'PK':('Tournament#' + (event['pathParameters']['tournament_id'])),'SK':('Stage#' + (event['queryStringParameters']['stage']))
             }
         )
         stageId = response['Item']
