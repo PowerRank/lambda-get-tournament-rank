32fb8b7301e9fa8e2aff68d347eb7004e25a8583
aahm2221 <aisha.ahmed1@yahoo.com>
2023-10-23 09:38:12 -0500

    add optional stage functionality

--- lambda_function.py
+++ lambda_function.py
@@ -9,27 +9,37 @@ dynamodb = boto3.resource('dynamodb')
 def lambda_handler(event, context):
     try:
         table = dynamodb.Table(os.environ['TABLE_NAME'])
-        print(event['pathParameters']['tournament_id'])
-        print(event['queryStringParameters']['stage'])
-        response = table.get_item(
-            Key={
-                'PK':('Tournament#' + event['pathParameters']['tournament_id']),
-                'SK':('Stage#' + event['queryStringParameters']['stage'])
-            },
-            ProjectionExpression='StageId'
-        )
-        stageId = response['Item']['StageId']
-
-        response = table.query(
-            IndexName=os.environ['POINTS_LSI_NAME'],
-            ScanIndexForward=False,
-            ProjectionExpression='TeamId,#n,Points',
-            KeyConditionExpression=Key('PK').eq('Stage#'+stageId),
-            ExpressionAttributeNames={'#n': 'Name'}
-        )
+        stageIds = []
+        if event['queryStringParameters']['stage']:
+            response = table.get_item(
+                Key={
+                    'PK':('Tournament#' + event['pathParameters']['tournament_id']),
+                    'SK':('Stage#' + event['queryStringParameters']['stage'])
+                },
+                ProjectionExpression='StageId,#n',
+                ExpressionAttributeNames={'#n': 'Name'}
+            )
+            stageIds.append([response['Item']['StageId'], response['Item']['Name']])
+        else:
+            response = table.query(
+                ProjectionExpression='StageId,#n',
+                KeyConditionExpression=Key('PK').eq('Tournament#' + event['pathParameters']['tournament_id']),
+                ExpressionAttributeNames={'#n': 'Name'}
+            )
+            for item in response['Items']:
+                stageIds.append([item['StageId'], item['Name']])
+        for id in stageIds:
+            response = table.query(
+                IndexName=os.environ['POINTS_LSI_NAME'],
+                ScanIndexForward=False,
+                ProjectionExpression='TeamId,#n,Points',
+                KeyConditionExpression=Key('PK').eq('Stage#'+id[0]),
+                ExpressionAttributeNames={'#n': 'Name'}
+            )
+            ranks = ranks.append({'Name':id[1],'Ranks':response['Items']})
         return {
             'statusCode': 200, 
-            'body':json.dumps(json_util.loads(response['Items']))
+            'body':json.dumps(json_util.loads(ranks))
         }
     except Exception as e:
         print(f'Exception: {e}')
